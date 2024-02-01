0f7a9565a1cc89a7359f44b9bd7e84e575b1f4fe
Bruno Cesar <cesarbruno53@gmail.com>
2024-02-01 10:16:21 -0300

    model tema

---
+++ src/models/Postagem.ts
@@ -0,0 +1,10 @@
+﻿import Tema from "./Tema";
+
+export default interface Postagem{
+id: number;
+conteudo: string;	
+anexo: string;	
+estado:number;
+data_hora: string;	
+tema: Tema | null;
+}
---
+++ src/models/Tema.ts
@@ -0,0 +1,8 @@
+import Postagem from "./Postagem";
+
+export default interface Tema {
+    id: number;
+    nome: string;
+    descricao: string;
+    postagem?: Postagem | null;
+}
