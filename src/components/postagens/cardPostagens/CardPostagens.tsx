c4b232439f13fa04fba70c92c79fd124862084ea
Victoria_Moraes <victoria.candido.mm@gmail.com>
2024-02-05 16:52:37 -0300

    excluindo componentes com css, e corrigindo cards

--- src/components/postagens/cardPostagens/CardPostagens.tsx
+++ src/components/postagens/cardPostagens/CardPostagens.tsx
@@ -1,41 +1,40 @@
-import { Link } from 'react-router-dom';
-import Postagem from '../../../models/Postagem';
-import './CardPostagens.css'; 
+import { Link } from 'react-router-dom'
+import Postagem from '../../../models/Postagem'
 
 interface CardPostagemProps {
-  postagem: Postagem;
+  postagem: Postagem
 }
 
-function CardPostagem({ postagem }: CardPostagemProps) {
+function CardPostagem({postagem}: CardPostagemProps) {
   return (
-    <div className='card-postagem'>
-      <div className='header'>
-        <div className="user-info">
-          <img src={postagem.usuario?.foto} className='user-avatar' alt="" />
-          <h3 className='user-name'>{postagem.usuario?.nome}</h3>
+    <div className='border-blue-100 border flex flex-col rounded overflow-hidden justify-between'>
+      <div>
+        <div className="flex w-full bg-gradient-to-b from-blue-500 to-blue-500 text-white py-2 px-4 items-center gap-4">
+          <img src={postagem.usuario?.foto} className='h-12 rounded-full ' alt="" />
+          <h3 className='text-lg font-bold text-center uppercase '>{postagem.usuario?.nome}</h3>
+        </div>
+        <div className='p-4 '>
+          <h4 className='text-lg font-semibold uppercase'>Postagem {postagem.id}</h4>
+          <p>{postagem.conteudo}</p>
+          <p>Anexo: {postagem.anexo}</p>
+          <p>Tema: {postagem.tema?.nome}</p>
+          <p>Data: {new Intl.DateTimeFormat(undefined, {
+                    dateStyle: 'full',
+                    timeStyle: 'medium',
+                    timeZone: 'America/Sao_Paulo'
+                  }).format(new Date(postagem.data_hora))}</p>
         </div>
       </div>
-      <div className='content'>
-        <h4 className='post-title'>Postagem {postagem.id}</h4>
-        <p>{postagem.conteudo}</p>
-        <p>Anexo: {postagem.anexo}</p>
-        <p>Tema: {postagem.tema?.nome}</p>
-        <p>Data: {new Intl.DateTimeFormat(undefined, {
-          dateStyle: 'full',
-          timeStyle: 'medium',
-          timeZone: 'America/Sao_Paulo'
-        }).format(new Date(postagem.data_hora))}</p>
-      </div>
-      <div className="actions">
-        <Link to={`/editarPostagem/${postagem.id}`} className='edit-link'>
-          <button className='action-button'>Editar</button>
+      <div className="flex">
+      <Link to={`/editarPostagem/${postagem.id}`} className='w-full text-slate-100 bg-opacity-90 bg-blue-400  hover:bg-blue-600 flex items-center justify-center py-2'>
+          <button>Editar</button>
         </Link>
-        <Link to={`/deletarPostagem/${postagem.id}`} className='delete-link'>
-          <button className='action-button'>Deletar</button>
+        <Link to={`/deletarPostagem/${postagem.id}`} className='text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
+          <button>Deletar</button>
         </Link>
       </div>
     </div>
-  );
+  )
 }
 
-export default CardPostagem;
+export default CardPostagem
